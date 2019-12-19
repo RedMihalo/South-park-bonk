52546e77362b4c603dbc182e5869af0b3f22d704
esesinho <esesinho@gmail.com>
2019-12-19 21:26:09 +0100

    WIP

---
+++ Assets/Level.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Level : MonoBehaviour
+{
+    public NODE_TYPE type;
+    public enum NODE_TYPE
+    {
+        FIGHT,
+        SHOP,
+        OTHER
+    }
+
+    void Start()
+    {
+        type = (NODE_TYPE)Random.Range(0, Enum.GetNames(typeof(NODE_TYPE)).Length - 1);
+
+        GameObject text = this.transform.Find("Text").gameObject;
+        text.GetComponent<Text>().text = type.ToString();
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/UI/Levels/" + type.ToString());
+    }
+    
+    
+
+}
+
+
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public int stages = 10;
+    public int maxOptions = 3;
+    public GameObject Level;
+    public List<List<GameObject>> map;
+    public int curentStage = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void setUp()
+    {
+        map = new List<List<GameObject>>();
+        for (int i = 0; i < stages; i++)
+        {
+            List<GameObject> stage = new List<GameObject>();
+            int amount = Random.Range(1, maxOptions);
+            for (int j = 0; j < amount; j++)
+            {
+                GameObject level = Instantiate(Level, this.transform) as GameObject;
+                level.SetActive(false);
+                stage.Add(level);
+            }
+            map.Add(stage);
+        }
+    }
+
+    public void changeStage()
+    {
+        foreach(GameObject level in map[curentStage])
+        {
+            level.SetActive(true);
+            curentStage++;
+        }
+    }
+    void Start()
+    {
+        setUp();
+        changeStage();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
