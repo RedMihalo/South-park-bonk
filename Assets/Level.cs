2dbea7ed3cf9181251b9732a23228d7476c74962
esesinho <esesinho@gmail.com>
2020-01-21 20:42:59 +0100

    some changes

--- Assets/Level.cs
+++ Assets/Level.cs
@@ -2,13 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
-
 public class Level : MonoBehaviour
 {
     public NODE_TYPE type;
     public List<Sprite> sprites;
+    public Button Button;
     public enum NODE_TYPE
     {
         FIGHT,
@@ -19,10 +20,21 @@ public class Level : MonoBehaviour
     void Start()
     {
         type = (NODE_TYPE)Random.Range(0, Enum.GetNames(typeof(NODE_TYPE)).Length);
-
+        if(type == NODE_TYPE.FIGHT)
+        {
+            Button.onClick.AddListener(()=>ChangeScene("SampleScene"));
+        }
+        if (type == NODE_TYPE.STORY)
+        {
+            Button.onClick.AddListener(() => ChangeScene("FreeRoamScene"));
+        }
         GameObject text = this.transform.Find("Text").gameObject;
         text.GetComponent<Text>().text = type.ToString();
         GetComponent<Image>().sprite = sprites[(int)type];
+    }
+    void ChangeScene(string Name)
+    {
+        SceneManager.LoadScene(Name);
     }
     
     
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,47 +7,21 @@ using Random = UnityEngine.Random;
 public class MapGenerator : MonoBehaviour
 {
 
-    public int stages = 10;
     public int maxOptions = 3;
     public GameObject Level;
-    public List<GameObject> map;
-    public int curentStage = 0;
-
-
-
 
     // Start is called before the first frame update
     void setUp()
     {
-        map = new List<GameObject>();
         int amount = Random.Range(1, maxOptions + 1);
         for (int j = 0; j < amount; j++)
         {
-            GameObject obj = Instantiate(Level, Vector2.zero, Quaternion.identity) as GameObject;
+            GameObject obj = Instantiate(Level, this.transform) as GameObject;
             obj.transform.SetParent(this.transform);
-            map.Add(obj);
         }
-    }
-
-    public void changeStage()
-    {
-        //for(int i = 0; i < this.transform.childCount; i++)
-        //{
-        //    this.transform.GetChild(i).gameObject.SetActive(false);
-        //}
-        this.transform.DetachChildren();
-        setUp();
     }
     void Start()
     {
         setUp();
-        //changeStage();
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
